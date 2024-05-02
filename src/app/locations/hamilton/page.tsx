ff1ee593d4ad2bf31deff635c77ad8cdede44f91
GlebTc <bob88tikhonov@gmail.com>
2024-05-02 19:54:51 -0400

    Added Toronto Location

---
+++ src/app/locations/hamilton/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Hamilton = () => {
+    const componentName = 'LOCATIONS_HAMILTON';
+  return (
+    <div className={`${componentName}_MAIN_CONTAINER`}>Hamilton</div>
+  )
+}
+
+export default Hamilton
