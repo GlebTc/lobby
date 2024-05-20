caa9ca59fd17ed2de8aaa71ecaa0b320cfa9af03
GlebTc <bob88tikhonov@gmail.com>
2024-05-20 13:00:44 -0400

    testing location picker

--- src/components/reusable/LocationPicker.tsx
+++ src/components/reusable/LocationPicker.tsx
@@ -15,56 +15,60 @@ const LocationPicker = () => {
 
   return (
     <div className={`${componentName}_MAIN_CONTAINER relative`}>
-      <div
-        className={
-          showLocationPicker
-            ? `${componentName}_OVERLAY fixed left-0 top-0 w-full h-screen bg-gray-800/90 duration-300`
-            : `${componentName}_OVERLAY hidden`
-        }
-      ></div>
-      <div
-        className={`${
-          showLocationPicker
-            ? `${componentName}_SIDE_MENU_CONTAINER fixed top-0 left-0 w-[300px] sm:w-[350px] h-full bg-[#fff9ef] duration-[var(--main-duration)]`
-            : `${componentName}_SIDE_MENU_CONTAINER fixed top-0 left-[-100%] w-[350px] h-full bg-[#fff9ef] duration-[var(--main-duration)]`
-        }`}
-      >
-        <div
-          className={`${componentName}_HEADER_CONTAINER relative h-[120px] w-[100%] border-b-[1px] border-[var(--secondary-color)] flex items-center justify-center`}
-        >
-          <div className='font-[600] text-[16px] tracking-wide'>
-            Choose A Location
+      {selectedMenuItem && (
+        <div className='relative'>
+          <div
+            className={
+              showLocationPicker
+                ? `${componentName}_OVERLAY fixed left-0 top-0 w-full h-screen bg-gray-800/90 duration-300`
+                : `${componentName}_OVERLAY hidden`
+            }
+          ></div>
+          <div
+            className={`${
+              showLocationPicker
+                ? `${componentName}_SIDE_MENU_CONTAINER fixed top-0 left-0 w-[300px] sm:w-[350px] h-full bg-[#fff9ef] duration-[var(--main-duration)]`
+                : `${componentName}_SIDE_MENU_CONTAINER fixed top-0 left-[-100%] w-[350px] h-full bg-[#fff9ef] duration-[var(--main-duration)]`
+            }`}
+          >
+            <div
+              className={`${componentName}_HEADER_CONTAINER relative h-[120px] w-[100%] border-b-[1px] border-[var(--secondary-color)] flex items-center justify-center`}
+            >
+              <div className='font-[600] text-[16px] tracking-wide'>
+                Choose A Location
+              </div>
+              <IoClose
+                onClick={handleCloseButtonClick}
+                size={25}
+                className='absolute right-5 top-[1/2] text-[var(--secondary-color)] cursor-pointer hover:text-black duration-[var(--main-duration)]'
+              />
+            </div>
+            <Link
+              href={
+                selectedMenuItem == 'reservations'
+                  ? 'https://www.opentable.ca/r/lobby-toronto'
+                  : `${selectedMenuItem}/toronto`
+              }
+              onClick={handleCloseButtonClick}
+              className='h-[70px] w-[100%] border-b-[1px] border-[var(--secondary-color)] flex items-center justify-center hover:text-[var(--secondary-color)] duration-[var(--main-duration)]'
+            >
+              Toronto
+            </Link>
+            <Link
+              href={
+                selectedMenuItem == 'reservations'
+                  ? 'https://www.opentable.ca/r/lobby-hamilton'
+                  : `${selectedMenuItem}/hamilton`
+              }
+              onClick={handleCloseButtonClick}
+              className='h-[70px] w-[100%] border-b-[1px] border-[var(--secondary-color)] flex items-center justify-center hover:text-[var(--secondary-color)] duration-[var(--main-duration)]'
+            >
+              Hamilton
+            </Link>
+            <NewsLetter />
           </div>
-          <IoClose
-            onClick={handleCloseButtonClick}
-            size={25}
-            className='absolute right-5 top-[1/2] text-[var(--secondary-color)] cursor-pointer hover:text-black duration-[var(--main-duration)]'
-          />
         </div>
-        <Link
-          href={
-            selectedMenuItem == 'reservations'
-              ? 'https://www.opentable.ca/r/lobby-toronto'
-              : `${selectedMenuItem}/toronto`
-          }
-          onClick={handleCloseButtonClick}
-          className='h-[70px] w-[100%] border-b-[1px] border-[var(--secondary-color)] flex items-center justify-center hover:text-[var(--secondary-color)] duration-[var(--main-duration)]'
-        >
-          Toronto
-        </Link>
-        <Link
-          href={
-            selectedMenuItem == 'reservations'
-              ? 'https://www.opentable.ca/r/lobby-hamilton'
-              : `${selectedMenuItem}/hamilton`
-          }
-          onClick={handleCloseButtonClick}
-          className='h-[70px] w-[100%] border-b-[1px] border-[var(--secondary-color)] flex items-center justify-center hover:text-[var(--secondary-color)] duration-[var(--main-duration)]'
-        >
-          Hamilton
-        </Link>
-        <NewsLetter />
-      </div>
+      )}
     </div>
   );
 };
